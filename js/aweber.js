df173406630357f5e6c5e3ec8cb74eb7471acb6f
Markus Mattsson <markusmattsson@MacBook-Air-som-tillhor-Markus.local>
2021-09-06 17:05:58 +0200

    copied old landing page

---
+++ js/aweber.js
@@ -0,0 +1,18 @@
+(function() {
+    var IE = /*@cc_on!@*/false;
+    if (!IE) { return; }
+    if (document.compatMode && document.compatMode == 'BackCompat') {
+        if (document.getElementById("af-form-791915812")) {
+            document.getElementById("af-form-791915812").className = 'af-form af-quirksMode';
+        }
+        if (document.getElementById("af-body-791915812")) {
+            document.getElementById("af-body-791915812").className = "af-body inline af-quirksMode";
+        }
+        if (document.getElementById("af-header-791915812")) {
+            document.getElementById("af-header-791915812").className = "af-header af-quirksMode";
+        }
+        if (document.getElementById("af-footer-791915812")) {
+            document.getElementById("af-footer-791915812").className = "af-footer af-quirksMode";
+        }
+    }
+})();
